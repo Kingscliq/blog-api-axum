c8b6489628762ced411db7883c51396f4ec1b05c
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2026-02-18 23:28:11 +0100

    chore: updated create post handler

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,38 @@ use axum::{
     extract::State,
     http::{Response, StatusCode, response},
     response::IntoResponse,
+    routing::post,
 };
 use chrono::{DateTime, Local};
 use serde_json::json;
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 use thiserror::Error;
-use tokio::sync::broadcast::error;
+use tokio::sync::Mutex;
 use uuid::Uuid;
 
+#[derive(Clone)]
 struct Post {
-    id: u8,
+    id: Uuid,
     title: String,
     image_url: String,
     content: String,
     description: String,
     created_at: DateTime<Local>,
     updated_at: DateTime<Local>,
-    author_id: u8,
 }
 
-struct PostRequest {
+struct CreatePostRequest {
     title: String,
     description: String,
     image_url: String,
+    content: String,
+}
+
+struct UpdatePostRequest {
+    title: Option<String>,
+    description: Option<String>,
+    image_url: Option<String>,
+    content: Option<String>,
 }
 
 struct PostResponse {}
@@ -35,8 +44,27 @@ struct BlogPosts {
     posts: HashMap<Uuid, Post>,
 }
 
+impl BlogPosts {
+    fn create_post(&mut self, post: Post) -> Result<(), AppError> {
+        if self.posts.contains_key(&post.id) {
+            Err(AppError::BadRequest(format!(
+                "Post with ID: {} already Exists",
+                post.id
+            )))
+        } else {
+            self.posts.insert(Uuid::new_v4(), post);
+
+            Ok(())
+        }
+    }
+}
+
+struct AppState {
+    post_state: Arc<Mutex<BlogPosts>>,
+}
+
+#[derive(Debug)]
 enum AppError {
-    // #[error("Resourse not found")]
     BadRequest(String),
     NotFound(String),
     InternalServer(String),
@@ -64,6 +92,34 @@ impl IntoResponse for AppError {
         (status, response_body).into_response()
     }
 }
+
+// Handlers
+
+async fn create_post_handler(
+    State(app_state): State<AppState>,
+    Json(post_request): Json<CreatePostRequest>,
+) -> Result<Json<Post>, AppError> {
+    let mut post_guard = app_state.post_state.lock().await;
+
+    let new_post = Post {
+        id: Uuid::new_v4(),
+        title: post_request.title,
+        content: post_request.content,
+        description: post_request.description,
+        created_at: Local::now(),
+        updated_at: Local::now(),
+        image_url: "https://placehold.net/400x400.png".to_string(),
+    };
+
+    if let Ok(_) = post_guard.create_post(new_post.clone()) {
+        Ok(Json(new_post))
+    } else {
+        Err(AppError::BadRequest(
+            "An Error Occured while creating post".to_string(),
+        ))
+    }
+}
+
 // async fn create_post(post: Post, author_id: u8) -> Result<&Post, String> {
 //     let new_post = Post {
 //         id: Uuid::new_v4(),
@@ -115,30 +171,37 @@ impl IntoResponse for AppError {
 //     }
 // }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
 
-    let id = Uuid::new_v4();
 
-    println!("the generated ID is: {id}")
+    let  global_state = AppState {
+        post_state: Arc<Mutex<BlogPosts>>
+    };
+
+
+    let blog = Post::new()
+
+
+
+    // // POST /posts - needs all post data
+    // struct CreatePostRequest {
+    //     title: String,
+    //     content: String,
+    //     description: String,
+    // }
+
+    // // PUT /posts/{id} - needs ID + updated data
+    // struct UpdatePostRequest {
+    //     title: Option<String>,  // Optional fields for partial updates
+    //     content: Option<String>,
+    //     description: Option<String>,
+    // }
+
+    // // GET /posts/search - needs search parameters
+    // struct SearchPostsRequest {
+    //     query: String,
+    //     limit: Option<u32>,
+    // }
 }
-
-// // POST /posts - needs all post data
-// struct CreatePostRequest {
-//     title: String,
-//     content: String,
-//     description: String,
-// }
-
-// // PUT /posts/{id} - needs ID + updated data
-// struct UpdatePostRequest {
-//     title: Option<String>,  // Optional fields for partial updates
-//     content: Option<String>,
-//     description: Option<String>,
-// }
-
-// // GET /posts/search - needs search parameters
-// struct SearchPostsRequest {
-//     query: String,
-//     limit: Option<u32>,
-// }
